0b4b1401de91092b779a6a684a7dbca4e8b1c3aa
Greg Arndt <arndt.greg@gmail.com>
2016-04-22 11:57:20 -0500

    Add array includes plugin to the rules

--- babel.js
+++ babel.js
@@ -3,10 +3,11 @@ module.exports = {
     'es2015',
   ],
   plugins: [
+    'array-includes',
     'syntax-async-functions',
+    'syntax-trailing-function-commas',
     'transform-runtime',
     'transform-async-to-generator',
     'transform-strict-mode',
-    'syntax-trailing-function-commas'
   ],
 }
